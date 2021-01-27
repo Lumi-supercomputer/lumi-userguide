7bd2ed62c3c5330c4c53b7e1fdc9fc8234966c63
alvaro.gonzalez <alvaro.gonzalez@csc.fi>
2021-01-27 10:53:38 +0200

    Add generation of default password

---
+++ entry-point.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ ! -f /etc/htpasswd/htpasswd ];
+then
+    echo "No '/etc/htpasswd/htpasswd' found, using default (user: 'admin', password: 'adminp')"
+    mkdir /etc/htpasswd/
+    htpasswd -cb /etc/htpasswd/htpasswd admin adminp
+else
+    echo "Found '/etc/htpasswd/htpasswd', using it."
+fi
+
+echo "Launching nginx"
+
+exec /usr/sbin/nginx
